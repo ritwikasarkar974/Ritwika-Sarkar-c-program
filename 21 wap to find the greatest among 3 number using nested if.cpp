d579bfccf3b77c1636a1943d4ddd32f0c346b0fe
ritwikasarkar974 <ritwikasarkar974@gmail.com>
2025-02-13 08:40:56 -0800

    Add files via upload

---
+++ 21 wap to find the greatest among 3 number using nested if.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("ENTER THE FIRST NUMBER");
+	scanf("%d",&a);
+	printf(" ENTER THE SECOND NUMBER");
+	scanf("%d",&b);
+	printf("ENTER THE THIRD NUMBER");
+	scanf("d",&c);
+	{
+		if (a>b&& a>c);
+		printf("a is tge greatest number");
+		elseif (b>a && b>c);
+		printf(" b is the greatest number");
+		else if(c>a && c>b);
+		printf("c is the greatest number");
+		
+	}
+	return 0;
+}
