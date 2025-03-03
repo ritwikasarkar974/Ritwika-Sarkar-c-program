ad824154c2da43f0bf8b8455e3418f82b7c0e3a0
ritwikasarkar974 <ritwikasarkar974@gmail.com>
2025-03-03 16:08:08 +0530

    Add files via upload

---
+++ assingment 37.cpp
@@ -0,0 +1,22 @@
+int main()
+#include<stdio.h>
+int main()
+{
+	int i,j,k;
+	for(i=1;i<=4;i++)
+	{
+	for(k=3;k>=i;k--);
+	{
+		printf(" ");
+		
+	}
+	for(j=1;j<=i;j++)
+	{
+		printf("*");
+		num++;
+	}
+	printf("\n");
+}
+return 0;
+}
+		
